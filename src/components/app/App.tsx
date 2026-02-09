90c7b191c7a51f4d14ffc6988ce060840ac05b73
Bilol <83568251+Bilol03@users.noreply.github.com>
2026-02-09 12:03:27 +0500

    React first lesson. Learning components

---
+++ src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from '../button/button'
+import { Field } from '../field/field'
+import { Header } from '../header/header'
+import './App.css'
+
+function App() {
+	return (
+		<div className="container">
+			<Header />
+			<Field/>
+			<Button />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/field/field.tsx
@@ -0,0 +1,5 @@
+export function Field() {
+    return <div>
+        <input type="text" placeholder="Enter text here" />
+    </div>
+}
