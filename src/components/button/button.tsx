c147c4266df12c17b55424c4efe81e3d7e94e13f
Bilol <83568251+Bilol03@users.noreply.github.com>
2026-02-16 14:59:00 +0500

    class based components

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,44 @@
-export function Button() {
-    return <div>
-        <button>Submit</button>
-    </div>
+import { Component } from 'react'
+import './button.css'
+
+// export function Button() {
+//     return <div>
+//         <button >Submit</button>
+//         <p>0</p>
+//     </div>
+// }
+
+export class Button extends Component {
+	constructor(props: any) {
+		super(props)
+		this.state = {
+			counter: 10,
+		}
+	}
+
+    increaseBtn = () => {
+        this.setState(prevState => ({
+            counter: prevState.counter += 1
+        }))
+    }
+    decreaseBtn = () => {
+        this.setState(prevState => ({
+            counter: prevState.counter > 0 ? prevState.counter -= 1 : 0
+        }))
+    }
+    restartBtn = () => {
+        this.setState(prevState => ({
+            counter: 0
+        }))
+    }
+	render() {
+		return (
+			<div>
+				<button  onClick={this.increaseBtn}>Increament</button>
+				<button onClick={this.decreaseBtn}>Decreament</button>
+				<button onClick={this.restartBtn}>Restart</button>
+				<p>{this.state.counter}</p>
+			</div>
+		)
+	}
 }
